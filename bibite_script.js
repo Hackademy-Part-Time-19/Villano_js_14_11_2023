3b446a3247e0be5843c449493ea41938702390ed
antvill22 <117175629+antvill22@users.noreply.github.com>
2023-11-15 18:52:36 +0100

    bibite

---
+++ bibite_script.js
@@ -0,0 +1,28 @@
+let coca_cola = 1
+let pepsi = 2
+let fanta = 3
+let sprite = 4
+let selezione = parseInt(prompt("seleziona bevanda: per coca-cola immettere 1, pepsi 2, fanta 3, sprite 4"))
+
+if (selezione===null || selezione.trim()===""){alert("inserire una selezione valida")}
+else{
+    switch (selezione) {
+        case 1:
+            console.log("erogazione bibita")
+        break;
+        case 2:
+            console.log("erogazione bibita")
+        break;
+        case 3:
+            console.log("erogazione bibita")
+        break;
+        case 4:
+            console.log("erogazione bibita")
+        default:
+        alert("selezione invalida")
+
+        break;
+    }
+
+
+}
